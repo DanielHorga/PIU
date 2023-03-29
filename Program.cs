e618dc1a5e3f29b215d577f639d8eda95c0727cf
eNJoy15 <danielhorga30@gmail.com>
2023-03-29 19:41:24 +0300

    Fisere pentru clase

--- Program.cs
+++ Program.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Program;
+
+
 namespace PIU
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var matchTime = new DateTime(2023, 09, 02, 21, 30, 0);
-            Match myMatch = new Match("TeamA", "TeamB", matchTime);
+            var timpMeci = new DateTime(2023, 09, 02, 21, 30, 0);
+            Meci meciNou = new Meci("TeamA", "TeamB", timpMeci);
 
-            string homeTeam = myMatch.HomeTeam; 
-            myMatch.AwayTeam = "TeamC"; 
+            string echipaAcasa = meciNou.EchipaAcasa; 
+            meciNou.EchipaDeplasare = "TeamC";
+            Console.ReadLine();
 
 
         }
